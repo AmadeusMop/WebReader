386d5133096f9ca2cbc211df23a30a997df36d44
Cillian Murphy <spacill@murphnduff.org>
2013-10-22 09:55:11 -0700

    Added a bunch of regex stuff

--- WebReader/src/webReader/Main.java
+++ WebReader/src/webReader/Main.java
@@ -1,14 +1,24 @@
 package webReader;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringBufferInputStream;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		try {
+			String html = getHTML(parseURL("http://en.wikipedia.org/wiki/Mandelbrot_Set"));
+			System.out.println(parseHTML(html));
+		} catch(IOException e) {
+			System.out.println(e);
+		}
 	}
 	
 	public static InputStream parseURL(String s) {
@@ -21,7 +31,31 @@ public class Main {
 		}
 	}
 	
-	public static String parseHTML(InputStream s) {
-		return "";
+	public static String getHTML(InputStream s) throws IOException {
+		byte[] barray = new byte[20];
+		byte[] temp;
+		int i = 0, b = s.read();
+		while(b != -1) {
+			if(i == barray.length) {
+				temp = Arrays.copyOf(barray, i+20);
+				barray = temp;
+			}
+			barray[i] = (byte)b;
+			i++;
+			b = s.read();
+		}
+		String str = new String(barray);
+		//str = 
+		return str;
+	}
+	
+	private static String parseHTML(String s) {
+		//System.out.println(s);
+		Pattern p = Pattern.compile("<body.*?>.*?</body>", Pattern.DOTALL);
+		Matcher m = p.matcher(s);
+		m.find();
+		s = s.substring(m.start(), m.end());
+		s.split("<p>|<a.*?>|<h.>|<.*?/>");
+		return s;
 	}
 }
